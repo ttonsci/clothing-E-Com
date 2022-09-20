4b79fc0495459824205cc35e1229436f15f34642
张超 <zhangchao@zhangchaodeMBP.lan>
2022-09-20 22:15:59 +1000

    created checkout page along with checkout items and checkout cart function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Authentication from "./routes/authentication/authentication.component";
 
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/check-out/checkout.component";
 
 
 
@@ -14,6 +15,7 @@ const App = () => {
       <Route index element={ <Home /> }/>
       <Route path='shop' element={ <Shop /> }/>
       <Route path='auth' element={ <Authentication /> }/>
+      <Route path='checkout' element={ <Checkout /> }/>
       </Route>
     </Routes>
  );
